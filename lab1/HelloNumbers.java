707c5574a75e516709f0d061a93404d9f508d113
unknown <2076403760@qq.com>
2023-05-13 00:04:27 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int Sum = 0;
+        while (x < 10) {
+            Sum=Sum+x;
+            System.out.print(Sum+" " );
+            x = x + 1;
+        }
+    }
+}
